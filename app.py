9079dee02bcea6553bdaa7e321cadc4b94dd62de
wassimmejri <159653226+wassimmejri@users.noreply.github.com>
2026-08-04 12:10:04 +0100

    fix: Update payment amount to 200.90

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ def pay():
     return jsonify(
         service=SERVICE_NAME,
         action="payment_processed",
-        amount=100.90,
+        amount=200.90,
         currency=CURRENCY,
         pod=socket.gethostname(),
         version=APP_VERSION
